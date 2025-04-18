02279c5abaef7068e7e8cbd9ac915ecc600189d8
ageu_dev19 <ageumenezes423@gmail.com>
2025-04-18 14:32:15 -0300

    Add new transactions table

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,14 @@ const app = fastify()
 
 app.get('/hello', async () => {
   const tables = await knex('sqlite_schema').select('*')
+
   return tables
 })
 
-app.listen({ port: 3333 }).then(() => console.log('HTTP Server Running!'))
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
